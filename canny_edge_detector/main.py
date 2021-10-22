a57e02d21c46e81500b93b8b31d1da59cbac1bd9
Atharva Bhagwat <atharva.bhagwat42@gmail.com>
2021-10-22 19:22:05 -0400

    Added test images

---
+++ canny_edge_detector/main.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+
+image = cv2.imread(os.path.join('input_images','coins.bmp'))
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imshow("Original", image)
+cv2.imshow("Blurred", blurred)
+wide = cv2.Canny(blurred, 10, 200)
+mid = cv2.Canny(blurred, 30, 150)
+tight = cv2.Canny(blurred, 240, 250)
+# show the output Canny edge maps
+cv2.imshow("Wide Edge Map", wide)
+cv2.imshow("Mid Edge Map", mid)
+cv2.imshow("Tight Edge Map", tight)
+cv2.waitKey(0)
