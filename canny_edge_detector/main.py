Revised code, final testing complete. - Updated .gitignore - We are now normalizing gradients and gradient magnitude - Corrected nms (changed coordinate referencing from xy plane to ij plane) - Corrected percentile calculation for thresholds - Optimized threshold calculation, gradient angle calculation - Cleaned and commented code

canny_edge_detector/main.py
@@ -20,18 +20,27 @@ To save:
 
 *************************************************************************************************************************
 
+Dependacy Installation: pip3 install -r requirements.txt
+
 Usage: python3 main.py <path__to_img_file>
 
 Example: python3 main.py input_images/House.bmp
 
 After execution, 'results' folder will be created containing sub-folders with results of input images.
+
+*************************************************************************************************************************
+
+Libraries Used:
+- os: Create folder structure for output images
+- cv2: Read input image, write output images
+- argparse: Parse arguments (Input image file path)
+- numpy: ndarray handling, magnitude calculation (sqrt(a^2+b^2)), gradient angle calculation, setting undefined pixel values to 0
 """
 
 import os
-import argparse
 import cv2
+import argparse
 import numpy as np
-from math import atan, degrees
 
 class CannyEdgeDetector():
     def __init__(self, img_path):
@@ -43,6 +52,7 @@ class CannyEdgeDetector():
         self.img_path = img_path
         self.img_filename = self.get_img_filename()
         self.output_folder = os.path.join('results', self.img_filename.split('.')[0])
+        self.pad = 0
         self.img = None
         self.smooth_img = None
         self.gradient_x = None
@@ -82,7 +92,7 @@ class CannyEdgeDetector():
                             [-1,-1,-1]]
                         )
 
-        # Sector map to quantize gradient angles depending on multiple
+        # Sector map to quantize gradient angles depending on multiple of 22.5
         self.SECTORS = {
             0:0,
             1:1,
@@ -97,9 +107,9 @@ class CannyEdgeDetector():
 
         # Map to calculate neighbors depending on the sector
         self.NEIGHBORS = {
-            0:{'l':(-1,0),'r':(1,0)},
-            1:{'l':(-1,1),'r':(1,-1)},
-            2:{'l':(0,-1),'r':(0,1)},
+            0:{'l':(0,-1),'r':(0,1)},
+            1:{'l':(1,-1),'r':(-1,1)},
+            2:{'l':(-1,0),'r':(1,0)},
             3:{'l':(-1,-1),'r':(1,1)}
         }
 
@@ -110,7 +120,7 @@ class CannyEdgeDetector():
         """Helper function to create directories if they don't exist
 
         Args:
-            directory (str): Directory path
+        directory (str): Directory path
         """
         if not os.path.isdir(directory):
             os.makedirs(directory)
@@ -127,12 +137,12 @@ class CannyEdgeDetector():
         return img_path_split[0]
             
     def read_img(self):
-        """Read image stored at img_path in grayscale
+        """Function to read image stored at img_path in grayscale
         """
         self.img = cv2.imread(self.img_path, 0)
     
     def write_img(self, filename, file):
-        """Saves image
+        """Saves image at out_path
 
         Args:
             filename (str): Filename to store images
@@ -142,25 +152,13 @@ class CannyEdgeDetector():
         cv2.imwrite(out_path, file)
         print(f'{out_path} saved...')
 
-    def slice_array(self, array, start_x, start_y, filter_length):
-        """Returns array slices to convolute
-
+    def update_padding(self, val):
+        """Helper function which keeps track of padding value
+            
         Args:
-            array (ndarray): Input array
-            start_x (int): Left top corner x coordinate to start slicing
-            start_y (int): Left top corner y coordinate to start slicing
-            filter_length (int): Length of the filter to stop slicing
-
-        Returns:
-            ndarray: Sliced array
+            val (int): Value is equal to len(filter)//2
         """
-        slice = []
-        for itr_i in range(start_x, start_x+filter_length):
-            row = []
-            for itr_j in range(start_y, start_y+filter_length):
-                row.append(array[itr_i][itr_j])
-            slice.append(row)
-        return np.array(slice)
+        self.pad += val
     
     def convolution(self, x, y):
         """Implementation of convolution operation
@@ -172,37 +170,31 @@ class CannyEdgeDetector():
         Returns:
             ndarray: Resultant of x*y; * -> convolution
         """
-        output = np.zeros(x.shape)
-        start_x = start_y = (len(y)//2)
-        for _ in range(x.shape[0]-len(y)+1):
-            for _ in range(x.shape[1]-len(y)+1):
-                output[start_x][start_y] = (self.slice_array(x, start_x-(len(y)//2),start_y-(len(y)//2), y.shape[0])*y).sum()
-                start_y += 1
-            start_x += 1
-            start_y = len(y)//2
+        x_shape = x.shape
+        y_shape = y.shape
+        output_shape = (x_shape[0]-y_shape[0]+1, x_shape[1]-y_shape[1]+1)
+        output = np.zeros(output_shape)
+        for itr_x in range(output_shape[0]):
+            for itr_y in range(output_shape[1]):
+                output[itr_x][itr_y] = (x[itr_x:itr_x+y_shape[0], itr_y:itr_y+y_shape[1]]*y).sum()
         return output
     
     def angle_calc(self):
         """Calculates gradient angle: tan inv (Gy/Gx)
         """
         self.gradient_angle = np.zeros(self.gradient_magnitude.shape)
-        for itr_x in range(self.gradient_angle.shape[0]):
-            for itr_y in range(self.gradient_angle.shape[1]):
-                if self.gradient_x[itr_x][itr_y] != 0:
-                    self.gradient_angle[itr_x][itr_y] = degrees(atan(self.gradient_y[itr_x][itr_y]/self.gradient_x[itr_x][itr_y]))
-                else:
-                    self.gradient_angle[itr_x][itr_y] = np.NAN
+        self.gradient_angle = np.rad2deg(np.arctan2(self.gradient_y, self.gradient_x))
+        # If angle is negative add 180 to make it positive
+        self.gradient_angle[self.gradient_angle < 0] += 180
     
     def get_sector(self, angle):
         """Returns sector value (0-3)
             Logic: As the sector wheel is same along the 0-180 degree line, 
             we reduce the range from 0-360 to 0-180 for the gradient angle
             We achieve this by subtracting it by 180 if it is greater than 180
-            In case where the gradient angle is -ve
-            we first add 360 to it and then subtract it by 180 if it is greater than 180
             We then divide this value by 22.5(as the sector is divided into smaller sectors of 22.5 degrees)
-            This value gives us the multiple which will further give us the sector using map
-            Usage of map reduces the complexity of the problem
+            This value gives us the multiple which will further give us the sector using dictionary
+            Usage of dictionary reduces the complexity of the problem
 
         Args:
             angle (float): Gradient angle for a pixel location
@@ -210,8 +202,6 @@ class CannyEdgeDetector():
         Returns:
             int: Sector value (0-3)
         """
-        if angle < 0:
-            angle += 360
         if angle > 180:
             angle -= 180
         angle = int(angle//22.5)
@@ -219,16 +209,12 @@ class CannyEdgeDetector():
 
     def quantize_angle(self):
         """Function iterates over the gradient angle array and calculates sector
-            for every pixel location. If the 3*3 mask goes outside the edges of the image,
-            the quantized value is set to nan
+            for every pixel location.
         """
         self.quantized_angle = np.zeros(self.gradient_magnitude.shape)
         for itr_x in range(self.quantized_angle.shape[0]):
             for itr_y in range(self.quantized_angle.shape[1]):
-                if not np.isnan(self.gradient_angle[itr_x][itr_y]):
-                    self.quantized_angle[itr_x][itr_y] = self.get_sector(self.gradient_angle[itr_x][itr_y])
-                else:
-                    self.quantized_angle[itr_x][itr_y] = np.NAN
+                self.quantized_angle[itr_x][itr_y] = self.get_sector(self.gradient_angle[itr_x][itr_y])
 
     def nms_compare(self, ind_x, ind_y, sector):
         """Function calculates neighbor coordinates and checks if the center pixel is maximum out of the neighbors
@@ -243,51 +229,81 @@ class CannyEdgeDetector():
         Returns:
             int: Gradient magnitude or 0
         """
-        neighbor_l = {'x':ind_x+self.NEIGHBORS[sector]['l'][0],'y':ind_y+self.NEIGHBORS[sector]['l'][1]}
-        neighbor_r = {'x':ind_x+self.NEIGHBORS[sector]['r'][0],'y':ind_y+self.NEIGHBORS[sector]['r'][1]}
-        if self.gradient_magnitude[ind_x][ind_y] > self.gradient_magnitude[neighbor_l['x']][neighbor_l['y']] and self.gradient_magnitude[ind_x][ind_y] > self.gradient_magnitude[neighbor_r['x']][neighbor_r['y']]:
+        # Calculation of neighbors depending on the sector
+        # NEIGHBOR dictionary is used for calculating indexes of the ndarray
+        neighbor_l = {'x':ind_x+self.NEIGHBORS.get(sector).get('l')[0],'y':ind_y+self.NEIGHBORS.get(sector).get('l')[1]}
+        neighbor_r = {'x':ind_x+self.NEIGHBORS.get(sector).get('r')[0],'y':ind_y+self.NEIGHBORS.get(sector).get('r')[1]}
+        if (self.gradient_magnitude[ind_x][ind_y] >= self.gradient_magnitude[neighbor_l.get('x')][neighbor_l.get('y')]) and (self.gradient_magnitude[ind_x][ind_y] >= self.gradient_magnitude[neighbor_r.get('x')][neighbor_r.get('y')]):
             return self.gradient_magnitude[ind_x][ind_y]
         else:
             return 0
 
-    def apply_threshold(self, x, threshold):
+    def apply_threshold(self, x, threshold_25, threshold_50, threshold_75):
         """Applies simple thresholding operation
             If magnitude < threshold, set the edgemap pixel value to 0
             Else set the edgemap pixel value to 255 (white pixel)
 
         Args:
             x (ndarray): Gradient magnitude after non-maxima suppression
-            threshold (float): Threshold value
+            threshold_25 (float): 25th percentile value from gradient magnitudes after nms (excluding magnitudes equal to 0)
+            threshold_50 (float): 50th percentile value from gradient magnitudes after nms (excluding magnitudes equal to 0)
+            threshold_75 (float): 75th percentile value from gradient magnitudes after nms (excluding magnitudes equal to 0)
 
         Returns:
-            ndarray: Edgemap created after applying simple threshold operation
+            ndarray: Edgemap created after applying simple threshold operation with threshold_25
+            ndarray: Edgemap created after applying simple threshold operation with threshold_50
+            ndarray: Edgemap created after applying simple threshold operation with threshold_75
         """
-        edge_map = np.zeros(x.shape)
+        edgemap_t25 = np.zeros(x.shape)
+        edgemap_t50 = np.zeros(x.shape)
+        edgemap_t75 = np.zeros(x.shape)
         for itr_x in range(x.shape[0]):
             for itr_y in range(x.shape[1]):
-                if x[itr_x][itr_y] < threshold:
-                    edge_map[itr_x][itr_y] = 0
+                if x[itr_x][itr_y] <= threshold_25:
+                    edgemap_t25[itr_x][itr_y] = 0
                 else:
-                    edge_map[itr_x][itr_y] = 255
-        return edge_map
+                    edgemap_t25[itr_x][itr_y] = 255
+
+                if x[itr_x][itr_y] <= threshold_50:
+                    edgemap_t50[itr_x][itr_y] = 0
+                else:
+                    edgemap_t50[itr_x][itr_y] = 255
+
+                if x[itr_x][itr_y] <= threshold_75:
+                    edgemap_t75[itr_x][itr_y] = 0
+                else:
+                    edgemap_t75[itr_x][itr_y] = 255
+        return edgemap_t25, edgemap_t50, edgemap_t75
 
     def gaussian_smoothing(self):
         """Gaussian smoothing operation: IMAGE * GAUSSIAN_FILTER; * -> convolution
         """
         self.smooth_img = self.convolution(self.img, self.GAUSSIAN_FILTER)/self.NORMALIZATION_FACTOR
-        self.write_img('smooth_'+self.img_filename, self.smooth_img)
+        self.update_padding(len(self.GAUSSIAN_FILTER)//2)
+        self.write_img('smooth_'+self.img_filename, np.pad(self.smooth_img, self.pad))
         
     def gradient_calc(self):
         """Calculate horizontal and vertical gradients using prewitt operator: IMAGE * PREWITT_X and IMAGE * PREWITT_y; * -> convolution
         """
         self.gradient_x = self.convolution(self.smooth_img, self.PREWITT_X)
         self.gradient_y = self.convolution(self.smooth_img, self.PREWITT_Y)
-        self.gradient_magnitude = np.sqrt(np.square(self.gradient_x)+np.square(self.gradient_y))
-        self.write_img('horizontal_'+self.img_filename, self.gradient_x)
-        self.write_img('vertical_'+self.img_filename, self.gradient_y)
-        self.write_img('magnitude_'+self.img_filename, self.gradient_magnitude)
+        self.update_padding(len(self.PREWITT_X)//2)
+
+        self.gradient_magnitude = np.hypot(self.gradient_x, self.gradient_y)
+
         self.angle_calc()
-        
+
+        # Normalizing gradients and gradient magnitude to set the range to [0, 255]
+        # Normalization formula: x/max(x) * 255
+        self.gradient_x = abs(self.gradient_x)/abs(self.gradient_x).max() * 255
+        self.gradient_y = abs(self.gradient_y)/abs(self.gradient_y).max() * 255
+        self.gradient_magnitude = self.gradient_magnitude/self.gradient_magnitude.max() * 255
+
+        self.write_img('horizontal_'+self.img_filename, np.pad(self.gradient_x, self.pad))
+        self.write_img('vertical_'+self.img_filename, np.pad(self.gradient_y, self.pad))
+        self.write_img('magnitude_'+self.img_filename, np.pad(self.gradient_magnitude, self.pad))
+
+
     def nms(self):
         """Non-maxima suppression function
             Steps:
@@ -296,32 +312,23 @@ class CannyEdgeDetector():
         """
         self.quantize_angle()
         self.magnitude_nms = np.zeros(self.gradient_magnitude.shape)
-        for itr_x in range(self.magnitude_nms.shape[0]):
-            for itr_y in range(self.magnitude_nms.shape[1]):
-                if not np.isnan(self.quantized_angle[itr_x][itr_y]):
-                    self.magnitude_nms[itr_x][itr_y] = self.nms_compare(itr_x, itr_y, self.quantized_angle[itr_x][itr_y])
-                else:
-                    self.magnitude_nms[itr_x][itr_y] = 0
+        for itr_x in range(1, self.magnitude_nms.shape[0]-1):
+            for itr_y in range(1, self.magnitude_nms.shape[1]-1):
+                self.magnitude_nms[itr_x][itr_y] = self.nms_compare(itr_x, itr_y, self.quantized_angle[itr_x][itr_y])
+        self.magnitude_nms = np.pad(self.magnitude_nms, self.pad)
         self.write_img('nms_magnitude_'+self.img_filename, self.magnitude_nms)
 
     def thresholding(self):
         """Simple thresholding operation
             We calculate 3 thresholds:
-                1) T_25 = 25th percentile of magnitude after nms
-                2) T_50 = 50th percentile of magnitude after nms
-                3) T_75 = 75th percentile of magnitude after nms
+                1) T_25 = 25th percentile of magnitude after nms (excluding magnitude equal to 0)
+                2) T_50 = 50th percentile of magnitude after nms (excluding magnitude equal to 0)
+                3) T_75 = 75th percentile of magnitude after nms (excluding magnitude equal to 0)
         """
-        threshold_25 = np.percentile(list(set(self.magnitude_nms.flatten())),25)
-        threshold_50 = np.percentile(list(set(self.magnitude_nms.flatten())),50)
-        threshold_75 = np.percentile(list(set(self.magnitude_nms.flatten())),75)
+        magnitude_vals = [value for value in self.magnitude_nms.flatten() if value != 0]
+        threshold_25, threshold_50, threshold_75 = np.percentile(magnitude_vals,[25, 50, 75])
 
-        print(threshold_25)
-        print(threshold_50)
-        print(threshold_75)
-
-        self.edgemap_t25 = self.apply_threshold(self.magnitude_nms, threshold_25)
-        self.edgemap_t50 = self.apply_threshold(self.magnitude_nms, threshold_50)
-        self.edgemap_t75 = self.apply_threshold(self.magnitude_nms, threshold_75)
+        self.edgemap_t25, self.edgemap_t50, self.edgemap_t75  = self.apply_threshold(self.magnitude_nms, threshold_25, threshold_50, threshold_75)
 
         self.write_img('edgemap_t25_'+self.img_filename, self.edgemap_t25)
         self.write_img('edgemap_t50_'+self.img_filename, self.edgemap_t50)
@@ -331,7 +338,7 @@ class CannyEdgeDetector():
         """Calls canny edge detection functions in order:
             - Read image
             - Gaussian smoothing
-            - Gradient calculation using prewitt's operator
+            - Gradient and gradient magnitude calculation using prewitt's operator
             - Non-maxima suppression
             - Thresholding
         """